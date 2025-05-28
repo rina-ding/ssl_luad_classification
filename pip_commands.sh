00d130a0aec5feaf3f63a0356d2ed7d7a0a4e29c
Rina Ding <rinading@rinas-air-2.ad.medctr.ucla.edu>
2025-05-28 13:31:35 -0700

    update

--- pip_commands.sh
+++ pip_commands.sh
@@ -6,6 +6,5 @@ pip install scikit-image
 pip install natsort
 pip install segmentation_models_pytorch
 cd preprocessing
-git clone https://github.com/getspams/spams-python
 cd spams-python
 pip install -e .
